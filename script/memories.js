8ab05c9116f8cfec0f24297cfa6f293d47acbd4e
Hisholo <haruhirokurosagi007@gmail.com>
2023-07-17 01:25:17 +0530

    'version1'

--- script/memories.js
+++ script/memories.js
@@ -1,9 +1,24 @@
+let precious;
 const getPreciousPics = function() {
     const domain = 'https://babefastapi-1-g2550338.deta.app';
     fetch(`${domain}/babe/precious`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => precious = data)
     .catch(error => alert(error))
-} 
+}
+
+const preciousContainer = document.querySelector('.preciousContainer');
+const displayPreciousPic = function(pic) {
+    const html = `
+    <div class="precious">
+        <img class="pic" src="${pic}"/>
+    </div>
+    `;
+    preciousContainer.insertAdjacentHTML('beforeend',html);
+}
 
 getPreciousPics();
+for(let i = 0; i < precious.length; i++)
+    displayPreciousPic(precious[i]);
+
+
